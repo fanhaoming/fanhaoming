8d5fcdaf58969ed3331362cca8470d4c106ab5f1
fanhaoming <393402303@qq.com>
2017-07-25 17:39:35 +0800

    1

---
+++ shirodemo/shiro/src/main/java/com/fhm/util/DbUtil.java
@@ -0,0 +1,46 @@
+package com.fhm.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * ���ݿ⹤����
+ * @author 
+ *
+ */
+public class DbUtil {
+
+	/**
+	 * ��ȡ���ݿ�����
+	 * @return
+	 * @throws Exception
+	 */
+	public Connection getCon() throws Exception{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_shiro", "root", "123456");
+		return con;
+	}
+	
+	/**
+	 * �ر����ݿ�����
+	 * @param con
+	 * @throws Exception
+	 */
+	public void closeCon(Connection con)throws Exception{
+		if(con!=null){
+			con.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DbUtil dbUtil=new DbUtil();
+		try {
+			dbUtil.getCon();
+			System.out.println("���ݿ����ӳɹ�");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("���ݿ�����ʧ��");
+		}
+	}
+}
